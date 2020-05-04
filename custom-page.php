c2c29416bab6dbfd2dcb398110bfea29c362428e
Sabian <58588133+SabianF@users.noreply.github.com>
2020-05-03 23:08:51 -0700

    Update custom-page.php

--- custom-page.php
+++ custom-page.php
@@ -5,7 +5,7 @@
      * Template Name: (PHP) Test Database
      * Template Post Type: post, page
      */
-    include '/home2/sabian/public_html/sf-includes/posts/test_database.php';
+    include 'inc/posts/test_database.php';
     
     //nonfunctional
     //$page_loci = '/home2/sabian/public_html/sf-includes/posts/' . get_post_content(get_post(get_the_ID()));
