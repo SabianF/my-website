017d215db7d916be493a161b65e3d09e5c633a85
Sabian <58588133+SabianF@users.noreply.github.com>
2020-05-04 00:04:20 -0700

    Moved db code to function

--- inc/posts/test_database.php
+++ inc/posts/test_database.php
@@ -1,12 +1,10 @@
 <?php
     /**
-     * STATUS = CONSTRUCTION
+     * STATUS = ALL OVER AGAIN
      *
      * @author Sabian Finogwar
      * @since 1.0.0
      */
-    include_once '../auth/sabian_eg_workout.php';
-    
     if ( ! defined( 'ABSPATH' ) ) {
     	exit; // Exit if accessed directly.
     }
@@ -21,13 +19,19 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="/sf-includes/styles.css">
+    <link rel="stylesheet" type="text/css" href="/wp-content/themes/astra-child/style.css">
 </head>
 <body>
 	<div id="primary" <?php astra_primary_class(); ?>>
 <?php
         astra_primary_content_top();
 ?>
+        <p class="center">Current working directory:<br>
+            <?php
+                echo "( ".getcwd().' )<br>';
+                path_exists("wp-content/themes/astra-child/style.css");
+            ?>
+        </p>
         <h1 class="center" style="padding-top:50px;"><?php get_the_title(get_post    (get_the_ID())) ?></h1>
         
         <div id="primary" class="content-area primary" >
@@ -36,26 +40,24 @@
                 <p class="center" style="padding:10px;">
                     We're still working on this functionality but don't worry, we'll have the rest of it ready, soon! Check back later!
                 </p>
-                <span id="todos" style="padding:10px;">
-                    Coming soon:
-                </span><!-- #todos -->
-                <div id="todo-list" style="margin-left:150px;margin-right:150px;">
-                    <ul>
-                        <li>Search and sort by date, type, count points</li>
-                        <li>Update & add new entries</li>
-                        <li>Visualizations</li>
-                    </ul>
-                </div><!-- #todo-list -->
             </div><!-- #const -->
             <div class="columns-2" style="margin-bottom:100px;">
                 <div id="table-forms">
                     <h2 class="center">Testing Area</h2>
                     <p class="center">Currently testing forms to search within database.</p>
+                    <div id="todos">
+                        Coming soon:
+                        <ul>
+                            <li>Search and sort by date, type, count points</li>
+                            <li>Update & add new entries</li>
+                            <li>Visualizations</li>
+                        </ul>
+                    </div><!-- #todos -->
                     <!-- TODO: Table Search/Entry Forms -->
                     <div class="columns-2">
                         <div style="padding: 5%;">
                             <h3>Test Form Filter</h3>
-                            <form action "/home2/sabian/public_html/sf-includes/auth/sabian_eg_workout_form.php" method="POST">
+                            <form action="wp-content/themes/astra-child/inc/auth/sabian_eg_workout_form.php" method="POST">
                                 <label id="form-label" for ="date">Date:</label><br>
                                 <input type="date" id="date" name="date"><br>
                                 <label id="form-label" for ="type">Type:</label><br>
@@ -65,12 +67,12 @@
                                     <option value="EKCx10"> EKCx10  </option>
                                     <option value="WPHx10"> WPHx10  </option>
                                 </select><br>
-                                <button id="form-button" type="submit" name="submit" action "/home2/sabian/public_html/sf-includes/auth/sabian_eg_workout_form.php" method="POST">Submit</button>
+                                <button id="form-button" type="submit" name="submit">Filter</button>
                             </form>
                         </div>
                         <div style="padding: 5%;">
                             <h3>Test Form Sort</h3>
-                            <form>
+                            <form action="wp-content/themes/astra-child/inc/auth/sabian_eg_workout_form.php">
                                 <label id="form-label" for ="type">Type:</label><br>
                                 <select id="type">
                                     <option value="Date">  Date     </option>
@@ -78,67 +80,16 @@
                                     <option value="Count"> Count    </option>
                                     <option value="Points">Points   </option>
                                 </select><br>
-                                <button id="form-button" type="button" name="submit">Submit</button>
+                                <button id="form-button" type="submit" name="submit">Sort</button>
                             </form>
                         </div>
                     </div><!-- #column -->
                 </div>
                 <div class="center" id="table-display" style="height:500px    ;overflow:scroll;padding-left:10px;padding-right:10px;">
-<?php   
-                	$sql = 'SELECT * FROM log_view;';
-                	$res = mysqli_query($conn, $sql);
-                	$chk = mysqli_num_rows($res);
-                	$non = 'Database not found (line 57) (include line 12)';
-                	$err = 'mysqli error (line 57)';
-                	
-                	if($chk == 0)
-                	{
-                	    echo $non;
-                	}
-                	else if ($chk > 0)
-                    {
-?>                      <!-- display db table column titles -->
-        	            <table class="t01" border="0" cellspacing="2"                 cellpadding="2"> 
-            	            <tr> 
-            	                <th>Date    </th> 
-            	                <th>Type    </th> 
-            	                <th>Count   </th> 
-            	                <th>Points  </th> 
-            	            </tr>
-<?php       	    
-            	            //display db table data
-            	            while ($row = mysqli_fetch_assoc($res))
-            	            {
-            	                $field1name = $row["Date"   ];
-            	                $field2name = $row["Workout"];
-            	                $field3name = $row["Count"  ];
-            	                $field4name = $row["Points" ];
-    
-            	                echo
-            	                '
-            	                    <tr> 
-            	                        <td>'.$field1name.'</td>
-            	                        <td>'.$field2name.'</td>
-            	                        <td>'.$field3name.'</td>
-            	                        <td>'.$field4name.'</td>
-            	                    </tr>
-            	                ';
-            	            }
-?>  	        
-            	        </table><!-- t01 -->
-<?php	        
-                	}
-                	else
-                	{
-                	    echo $err;
-                	}
-?>  
+                    <?php db_display(); ?>
                 </div><!-- #table-display -->
-	    </div><!-- #columns-2 -->
+        	</div><!-- #columns-2 -->
 <?php
-            mysqli_free_result($res);
-            mysqli_close($conn);
-            
             the_content();
 	        astra_primary_content_bottom();
 ?>  
