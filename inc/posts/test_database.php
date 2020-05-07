ea05d7fc37b69a2307f57dddcdefe4e1d9fdbecc
Sabian <58588133+SabianF@users.noreply.github.com>
2020-05-06 22:07:56 -0700

    Starting AJAX table column sort

--- inc/posts/test_database.php
+++ inc/posts/test_database.php
@@ -19,12 +19,19 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="/wp-content/themes/astra-child/style.css">
+    <link
+        rel="stylesheet"
+        type="text/css"
+        href="/wp-content/themes/astra-child/style.css">
+	<link
+	    rel="stylesheet"
+	    href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+	    integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
+	    crossorigin="anonymous">
     <script
 	  src="https://code.jquery.com/jquery-3.5.1.min.js"
 	  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-	  crossorigin="anonymous">
-    </script>
+	  crossorigin="anonymous"></script>
     <script src="/wp-content/themes/astra-child/inc/js/script.js"></script>
 </head>
 <body>
