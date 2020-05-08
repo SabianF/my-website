b62b3c30564a4a7b01ec14a8e32ecca2fdff27a8
Sabian <58588133+SabianF@users.noreply.github.com>
2020-05-07 23:53:33 -0700

    script.js AJAX table sort WIP

--- inc/js/script.js
+++ inc/js/script.js
@@ -79,22 +79,34 @@ $(document).ready(function(){
     
     $("th .column_sort").click(function()
     {
-        var nam = $("th .column_sort").attr('id');
-        /*
-        // AJAX Code To Submit Form.
+        var column_name = $(this).attr("id");
+        var order = $(this).data("order");
+        var arrow = '';
+        
+        //glyphicon glyphicon-arrow-up
+        //glyphicon glyphicon-arrow-down
+        if(order == 'desc')  
+        {  
+            arrow = '&nbsp;<span class="glyphicon glyphicon-arrow-do></span>';  
+        }  
+        else  
+        {  
+            arrow = '&nbsp;<span class="glyphicon glyphicon-arrow-up"></span>';  
+        }
+        
+        //TODO: column_name and order have proper value, but AJAX not passing value to url
         $.ajax
-        ({
-            type: "POST",
-            url: "ajaxsubmit.php",
-            data: dataString,
-            cache: false,
-            success: function(result)
-            {
-                alert(result);
-            }
-        });
-        */
-        alert("Clicked "+nam);
+        ({  
+            url:"/wp-content/themes/astra-child/inc/auth/sabian_eg_workout_sort.php",  
+            method:"POST",  
+            data:{column_name:column_name, order:order},  
+            success:function(data)  
+            {  
+                $('#table-display').html(data);  
+                $('#'+column_name+'').append(arrow);  
+            }  
+        })
+        alert(column_name+" "+order);
         return false;
     });
 });
