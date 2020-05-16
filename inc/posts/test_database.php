73419eed7159c018119c7de927b71c2a16536d5f
Sabian <58588133+SabianF@users.noreply.github.com>
2020-05-15 22:04:23 -0700

    test_database.php add data form DONE

--- inc/posts/test_database.php
+++ inc/posts/test_database.php
@@ -41,8 +41,6 @@ T<?php
 
 	<!-- DataTables -->
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.21/af-2.3.5/b-1.6.2/b-colvis-1.6.2/b-flash-1.6.2/b-html5-1.6.2/b-print-1.6.2/fh-3.1.7/r-2.2.4/sc-2.0.2/sp-1.1.0/sl-1.3.1/datatables.min.css"/>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.21/af-2.3.5/b-1.6.2/b-colvis-1.6.2/b-flash-1.6.2/b-html5-1.6.2/b-print-1.6.2/fh-3.1.7/r-2.2.4/sc-2.0.2/sp-1.1.0/sl-1.3.1/datatables.min.js"></script>
     
     <!-- Leaflet -->
@@ -70,7 +68,7 @@ T<?php
                 <p>
                     Coming soon:
                     <ul>
-                        <li>Search and sort by date, type, count points</li>
+                        <li><i class="fas fa-check"></i> Search and sort by date, type, count points</li>
                         <li>Update & add new entries</li>
                         <li>Visualizations</li>
                     </ul>
@@ -83,7 +81,25 @@ T<?php
                 </div><!-- #table-display -->
                 <div class="flex-child" id="table-forms">
                     <h2 class="center">Testing Area</h2>
-                    <p class="center">Currently building functionality to change, add new, and delete data.</p>
+                    <p class="center">Currently building functionality to change, add new, and delete data.</p><br>
+                    <div class="center" id="table-form-fields">
+                        <h4 id="addRow">Add New Entry</h3><br>
+                        <form id="addData">
+                            <label id="lb_date">Date</label><br>
+                            <input type="date" id="date_input"><br>
+                            <br>
+                            <label id="lb_workout">Workout</label><br>
+                            <input type="text" id="workout_input"><br>
+                            <br>
+                            <label id="lb_count">Count</label><br>
+                            <input type="number" id="count_input"><br>
+                            <br>
+                            <label id="lb_points">Points</label><br>
+                            <input type="number" id="points_input"><br>
+                            <br>
+                            <button type="button" id="addData_button">Add Submission</button>
+                        </form>
+                    </div><!-- #table-form-fields -->
                 </div><!-- #table-forms -->
         	</div><!-- #table-section -->
 <?php
