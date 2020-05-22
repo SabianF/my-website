84f130e23f5c3f5a67fd9339f3831c0d861d58a6
Sabian <58588133+SabianF@users.noreply.github.com>
2020-05-21 17:59:14 -0700

    script.js tablehighlights DONE CellsEdit WIP

--- inc/js/script.js
+++ inc/js/script.js
@@ -7,17 +7,48 @@ $(document).ready(function(){
         "paging": false
     });//$('#workout_table').DataTable
     
+    $('#workout_table tbody tr').on('mouseenter',function(){
+        $(this).css('background-color','#EEE');
+    });
+    $('#workout_table tbody tr td').on('mouseenter',function(){
+        $(this).css('background-color','#DDD');
+    });
+    $('#workout_table tbody tr').on('mouseleave',function(){
+        $(this).css('background-color','');
+    });
+    $('#workout_table tbody tr td').on('mouseleave',function(){
+        $(this).css('background-color','');
+    });
+    $('#workout_table tbody tr td').on('click',function(){
+        alert('Clicked: '+$(this).text()+". Editing functionality coming soon...");
+        //t.row(this).edit();
+    });
+    /*
+    function myCallbackFunction (updatedCell, updatedRow, oldValue) {
+        console.log("The new value for the cell is: " + updatedCell.data());
+        console.log("The values for each cell in that row are: " + updatedRow.data());
+        alert("Done!!!");
+    }
+
+    t.MakeCellsEditable({
+        "onUpdate": myCallbackFunction
+    });
+    */
     //Allow adding of new rows to #workout_table
     $('#addData_button').on('click',function(){
-        alert("Data submitted!");
+        var di = $('#date_input'    ).val();
+        var wi = $('#workout_input' ).val();
+        var ci = $('#count_input'   ).val();
+        var pi = $('#points_input'  ).val();
         
         t.row.add([
-            $('#date_input'     ).val()
-            ,$('#workout_input' ).val()
-            ,$('#count_input'   ).val()
-            ,$('#points_input'  ).val()
+             di
+            ,wi
+            ,ci
+            ,pi
         ]).draw(false);
         
+        alert('Successfully added '+(di?di:'')+(di&&wi?', '+wi:(wi?wi:''))+(wi&&ci?', '+ci:(ci?ci:''))+(ci&&pi?', '+pi:(pi?pi:'')));
         $('#addData').trigger("reset");
         
     });//$('#addData_button').on('click',function()
